be8a5cefcdc5d8ef39787a6bb1fe5a3eeba7ccc8
Ankit <ankitrajdihara123@gmail.com>
2025-09-20 08:48:39 +0530

    sol_faucet

---
+++ Token_launchpad/sol_faucet/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { FC, useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+
+// Default styles that can be overridden by your app
+import "@solana/wallet-adapter-react-ui/styles.css";
+import Request_airdrop from "./Request_airdrop";
+
+function App() {
+  return (
+    <ConnectionProvider
+      endpoint={
+        "https://solana-devnet.g.alchemy.com/v2/1PzJ64B2r79Nj3YnAqNbOvwn217xVMmd"
+      }
+    >
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          <WalletDisconnectButton />
+          <Request_airdrop />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
+
+export default App;

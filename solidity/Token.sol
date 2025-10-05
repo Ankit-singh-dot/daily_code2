515d0db84d76b569317e4b7d1329a227823cd934
Ankit <ankitrajdihara123@gmail.com>
2025-10-05 09:35:21 +0530

    allowance

---
+++ solidity/Token.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+contract BerryToken{
+   address public owner;
+   mapping (address => uint) public balances;
+   mapping (address => mapping(address=>uint)) public allowance;
+   uint public totalSupply; 
+   constructor() {
+    owner = msg.sender;
+   }
+
+   modifier onlyOwner (){
+    require(owner == msg.sender);
+    _;
+   }
+   function approve(address _spender, uint _value)public  returns (bool success){
+    allowance[msg.sender][_spender] = _value;
+    return true;
+   }
+   
+   function mint( uint amount) public onlyOwner{
+       balances[owner] += amount;
+       totalSupply += amount;
+   }
+   function mintTo(uint amount, address to) public onlyOwner {
+balances[to] += amount;
+totalSupply += amount;
+
+   }
+   function transfer(address to, uint amount) public{
+    uint existingBalance = balances[msg.sender];
+    require(existingBalance>=amount);
+       balances[msg.sender] -= amount;
+       balances[to] += amount;
+   }
+//    function burn(address to, uint amount) public onlyOwner{
+//        balances[to] -= amount;
+//    }
+    
+}

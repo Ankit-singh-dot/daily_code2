b8f4543d71724ee1523f7043711bbb3fed78bde7
Ankit <ankitrajdihara123@gmail.com>
2025-11-22 13:14:22 +0530

    proxy_sol

---
+++ solidity/replit_eth/Delegate3.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20; 
+contract StorageProxy{
+    uint public num;
+    address implementation;
+
+    constructor (address _implementation) {
+        num =0 ;
+        implementation = _implementation;
+    }
+    function setNum (uint _num )public {
+        (bool success,) =implementation.delegatecall(
+            abi.encodeWithSignature("setNum(256)", _num)
+        );
+        require(success,"Error while delegating call");
+    }
+    function setImplementaiton(address _implementation) public {
+        implementation = _implementation;
+    }
+} 
+
+
+contract Implementation{
+    uint public num;
+    function setNum(uint _num)public {
+        num = _num*2;
+    }
+}
+
+contract Implementation2{
+    uint public  num;
+    function setNum(uint _num) public  {
+        num = _num*3;
+    }
+}
+
+contract Implementation3{
+    uint public  num;
+    function SetNum (uint _num) public {
+        num = _num*5;
+    }
+}

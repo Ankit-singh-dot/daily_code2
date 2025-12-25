c05d513a4a4f589b87e7b01d82de3dfad4c62954
Ankit <ankitrajdihara123@gmail.com>
2025-12-25 10:42:06 +0530

    form_done

--- intern /frontend/src/pages/HomePage.jsx
+++ intern /frontend/src/pages/HomePage.jsx
@@ -1,11 +1,44 @@
 import React from "react";
-
+import { useState } from "react";
+import { useCallback } from "react";
 const HomePage = () => {
+  const [email, setEmail] = useState("");
+  const [room, setRoom] = useState("");
+  const handleSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!email || !room) {
+        alert("Please fill all fields");
+        return;
+      }
+
+      console.log("Email:", email);
+      console.log("Room:", room);
+    },
+    [email, room]
+  );
   return (
-    <div className="">
-      <input type="email" placeholder="enter ur email"></input>
-      <input type="text" placeholder="enter ur room id "></input>
-      <button>join room </button>
+    <div>
+      <form onSubmit={handleSubmitForm}>
+        {/* <label htmlFor="email">Email Id</label> */}
+        <input
+          type="email"
+          id="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        {/* <label htmlFor="room">Room number </label> */}
+        <input
+          type="text"
+          id="room"
+          placeholder="enter ur room number "
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+        ></input>
+        <button>join</button>
+      </form>
     </div>
   );
 };

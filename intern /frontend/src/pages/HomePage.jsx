252661cfea3b3f32f20a6522b73ba4f332dc87ab
Ankit <ankitrajdihara123@gmail.com>
2025-12-25 10:09:40 +0530

    home_page

---
+++ intern /frontend/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="">
+      <input type="email" placeholder="enter ur email"></input>
+      <input type="text" placeholder="enter ur room id "></input>
+      <button>join room </button>
+    </div>
+  );
+};
+
+export default HomePage;

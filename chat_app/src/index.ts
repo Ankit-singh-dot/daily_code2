1a8217ecf635af6bd7e0a397883acdcccfa3b6ac
Ankit <ankitrajdihara123@gmail.com>
2026-02-03 05:10:11 +0530

    done_with_dual_room_chat

--- chat_app/src/index.ts
+++ chat_app/src/index.ts
@@ -1,21 +1,39 @@
-import { WebSocketServer, WebSocket } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
-let userCount = 0;
-let allSocket: WebSocket[] = [];
+interface User {
+  socket: WebSocket;
+  room: string;
+}
+const allSocket: User[] = [];
 wss.on("connection", (socket) => {
-  allSocket.push(socket);
-  userCount = userCount + 1;
-  console.log("user connected " + userCount);
   socket.on("message", (message) => {
-    console.log("message received" + message.toString());
-    // setTimeout(() => {
-    //   socket.send(message.toString() + "message send from the origin");
-    // }, 1000);
-    //  this upper code is for the reply of the origin not for all the things
+    // @ts-ignore
+    const parsedMessage = JSON.parse(message);
 
-    for (let i = 0; i < allSocket.length; i++) {
-      const s = allSocket[i];
-      s?.send(message.toString() + ": send from the another server ");
+    if (parsedMessage.type === "join") {
+      console.log("user joined room" + parsedMessage.payload.roomId);
+      allSocket.push({
+        socket,
+        room: parsedMessage.payload.roomId,
+      });
+    }
+
+    if (parsedMessage.type === "chat") {
+      console.log("user want to chat ")
+      let currentUserRoom = null;
+
+      for (let i = 0; i < allSocket.length; i++) {
+        if (allSocket[i]?.socket == socket) {
+          currentUserRoom = allSocket[i]?.room;
+        }
+      }
+      console.log("enfwongkprgn")
+      for (let i = 0; i < allSocket.length; i++) {
+         console.log("reached hee")
+        if (allSocket[i]?.room == currentUserRoom) {
+          allSocket[i]?.socket.send(parsedMessage.payload.message);
+        }
+      }
     }
   });
 });

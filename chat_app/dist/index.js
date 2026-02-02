26ecead63909d5f3c786bdf9d3e08cbff383e470
Ankit <ankitrajdihara123@gmail.com>
2026-02-03 03:53:51 +0530

    done_with_the_basic_tranfer_server

--- chat_app/dist/index.js
+++ chat_app/dist/index.js
@@ -1,15 +1,21 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 let userCount = 0;
-let allSockets = [];
+let allSocket = [];
 wss.on("connection", (socket) => {
+    allSocket.push(socket);
     userCount = userCount + 1;
     console.log("user connected " + userCount);
     socket.on("message", (message) => {
         console.log("message received" + message.toString());
-        setTimeout(() => {
-            socket.send(message.toString() + ": Sent from the server ");
-        }, 1000);
+        // setTimeout(() => {
+        //   socket.send(message.toString() + "message send from the origin");
+        // }, 1000);
+        //  this upper code is for the reply of the origin not for all the things
+        for (let i = 0; i < allSocket.length; i++) {
+            const s = allSocket[i];
+            s?.send(message.toString() + ": send from the server ");
+        }
     });
 });
 //# sourceMappingURL=index.js.map

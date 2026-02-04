4c8892ac5f8b3267a4fae5796537bd52ccbb8580
Ankit <ankitrajdihara123@gmail.com>
2026-02-05 02:39:10 +0530

    random

--- chat_app/dist/index.js
+++ chat_app/dist/index.js
@@ -1,20 +1,32 @@
-import { WebSocketServer, WebSocket } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
-let userCount = 0;
-let allSocket = [];
+const allSocket = [];
 wss.on("connection", (socket) => {
-    allSocket.push(socket);
-    userCount = userCount + 1;
-    console.log("user connected " + userCount);
     socket.on("message", (message) => {
-        console.log("message received" + message.toString());
-        // setTimeout(() => {
-        //   socket.send(message.toString() + "message send from the origin");
-        // }, 1000);
-        //  this upper code is for the reply of the origin not for all the things
-        for (let i = 0; i < allSocket.length; i++) {
-            const s = allSocket[i];
-            s?.send(message.toString() + ": send from the server ");
+        // @ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type === "join") {
+            console.log("user joined room" + parsedMessage.payload.roomId);
+            allSocket.push({
+                socket,
+                room: parsedMessage.payload.roomId,
+            });
+        }
+        if (parsedMessage.type === "chat") {
+            console.log("user want to chat ");
+            let currentUserRoom = null;
+            for (let i = 0; i < allSocket.length; i++) {
+                if (allSocket[i]?.socket == socket) {
+                    currentUserRoom = allSocket[i]?.room;
+                }
+            }
+            console.log("enfwongkprgn");
+            for (let i = 0; i < allSocket.length; i++) {
+                console.log("reached hee");
+                if (allSocket[i]?.room == currentUserRoom) {
+                    allSocket[i]?.socket.send(parsedMessage.payload.message);
+                }
+            }
         }
     });
 });

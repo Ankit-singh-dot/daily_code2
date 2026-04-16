e9c0871eab1c494ef933ce8c5f310016d2ac98f1
Ankit <ankitrajdihara123@gmail.com>
2026-04-16 12:05:36 +0530

    turbO_repo_cicd_pipline

---
+++ mono-repo-deployment/packages/db/src/index.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+const prismaClient = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+});
